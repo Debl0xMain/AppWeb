7114509320fc69c9dfe098cfd7a3005d02b481bb
Debl0xMain <deblox160999@gmail.com>
2024-03-21 15:45:48 +0100

    test core/service

--- src/Form/AdressFormType.php
+++ src/Form/AdressFormType.php
@@ -11,10 +11,21 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Validator\Constraints\Valid;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use App\Repository\UsersRepository;
+use Symfony\Component\userRepo\Core\userRepo;
+
+
 class AdressFormType extends AbstractType
 {
+    private $userRepo;
+
+    public function __construct(UsersRepository $userRepo){
+        $this->userRepo = $userRepo;
+    }
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $currentUser = $this->userRepo->getUser();
+
         $builder
             ->add('adrNumber',TextType::class,[
             'label_attr' => [
@@ -56,11 +67,21 @@ class AdressFormType extends AbstractType
                     'class'=>' adress_input ' ,
                     ]
         ])
-        ->add('users', EntityType::class, [
+        // ->add('users', EntityType::class, [
+        //     'class' => Users::class,
+        //     'choice_label' => 'id',
+        // ])
+        ->add('user', EntityType::class, [
             'class' => Users::class,
             'choice_label' => 'id',
+            'query_builder' => function (UsersRepository $er) use ($currentUser) {
+                return $er->createQueryBuilder('u')
+                    ->where('u.id = :userId')
+                    ->setParameter('userId', $currentUser->getId());
+            },
+            'multiple' => false,
+            'disabled' => true,
         ])
-
         ->add('save', SubmitType::class, [
             'attr'=> 
            [
