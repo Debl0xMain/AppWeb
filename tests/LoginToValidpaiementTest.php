c7d52e3008bd0d38cea27a6fc71aa651fd4d1a78
Debl0xMain <deblox160999@gmail.com>
2024-07-09 09:40:16 +0200

    ddd

--- tests/LoginToValidpaiementTest.php
+++ tests/LoginToValidpaiementTest.php
@@ -14,22 +14,22 @@ class LoginToValidpaiementTest extends PantherTestCase
 
 // Selecion le btn login Inscription
         $crawler->filter('.btnicon')->click();
-        sleep(1);
+        sleep(5);
 // switch vers login
         $crawler->filter('.slider')->click();
-        sleep(1);
+        sleep(5);
 //Log le client
         $form = $crawler->selectButton('Sign in')->form();
         $form['userEmail'] = 'particulier1@example.com';
         $form['password'] = 'admin';
     //sum le formulaire
         $crawler = $client->submit($form);
-        sleep(1);
+        sleep(5);
 
 // click Vents
         $crawler = $client->clickLink('Vents');
         $crawler->filter("#btn_28");
-        sleep(1);
+        sleep(5);
     // refesh crawler
         $updatedCrawler = $client->refreshCrawler();
 
@@ -37,32 +37,32 @@ class LoginToValidpaiementTest extends PantherTestCase
 //Open bash
         $updatedCrawler->filter('#shopbtn')->click();
 
-        sleep(1);
+        sleep(5);
 //sup article panier
         $updatedCrawler->filter('.btn-outline-danger')->click();
         $updatedCrawler->filter('.btn-outline-danger')->click();
-        sleep(1);
+        sleep(5);
 
 // click Sono
         $crawler = $client->clickLink('Sono');
         $crawler->filter("#btn_37");
-        sleep(1);
+        sleep(5);
         $updatedCrawler = $client->refreshCrawler();
         $updatedCrawler->filter('#btn_37')->click();
-        sleep(1);
+        sleep(5);
 //Open bash
         $updatedCrawler->filter('#shopbtn')->click();
-        sleep(1);
+        sleep(5);
 //ouverture du panier
         $updatedCrawler = $client->clickLink('Paiement');
         //go Paiement
         $updatedCrawler = $client->refreshCrawler();
-        sleep(1);
+        sleep(5);
         //Select condition livraison
 
         $myInput = $updatedCrawler->filterXPath(".//select[@id='adress_cmd_valid_livraison']//option[@value='1']");
         $myInput->click();
-        sleep(1);
+        sleep(5);
 
         $client->executeScript("document.querySelector('#paiement_btn').click()");
         $client->executeScript("document.querySelector('#request_valid_paiement').click()");
@@ -70,7 +70,7 @@ class LoginToValidpaiementTest extends PantherTestCase
         //changement de fenetre
         $handles = $client->getWindowHandles();
         $client->switchTo()->window(end($handles));
-        sleep(1);
+        sleep(5);
         $client->executeScript("document.querySelector('#send_data_paiement').click()");
         sleep(10);
         //retour a la fenetre
